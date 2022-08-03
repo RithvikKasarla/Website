a992b61ae1bfd5d604c7db7d20bdddc89b1447eb
RithvikKasarla <rithvikrk719@gmail.com>
2022-08-03 08:27:02 -0400

    css update and project description update

--- commands.js
+++ commands.js
@@ -220,35 +220,46 @@ function outputs(cmd) {
 
       var text = [
         "<h3>Terminal Website</h3>",
-        "A Personal Website designed like a terminal. It is designed with basic HTML, CSS,",
-        "and JS. The inspiration for this design was my use of linux as my main operating system.	",
+        "A Personal Website designed like a terminal. The inspiration for this design ",
+        "was to give the user a glimpse into my world as a programmer and using the.",
+        "terminal.",
+        "-> Created a frontend using HTML, CSS, and JavaScript",
+        "-> Deployed on an AWS s3 bucket",
+        "-> Domain was created from AWS route53",
+        "Tech: HTML, CSS, JavaScript, AWS Route53, AWS s3 buckets",
       ];
 
       addLines(text);
       setTimeout(() => {
         addNewCommandLine();
-      }, lineTime(text, text.length));
+      }, lineTime(text, text.length) + 400);
       break;
     case "project2":
       passedCommands.unshift(cmd);
       var text = [
         "<h3>Url Shortener</h3>",
-        "A website which can allow you to shorten long urls to a key of your choice. You",
-        "could also search through any of the existing shortened URLs and their long URL",
-        "counterparts to see any that already exist.",
+        "A webapp which allows users to shorten any long URL. Users can navigate to the website ",
+        "using the shortened URL, or search through previously created shortened URLs.",
+        "-> Developed the application using a ReactJs frontend and tailwind’s CSS framework",
+        "-> Designed and built a Rest API using ExpressJs as backend and Mongo DB database",
+        "-> Deployed on an AWS EC2 Instance with a Nginx webserver ",
+        "Tech: ReactJs, Rest API, ExpressJs, Tailwind CSS, Mongo DB, AWS EC2, Nginx ",
       ];
       addLines(text);
       setTimeout(() => {
         addNewCommandLine();
-      }, lineTime(text, text.length));
+      }, lineTime(text, text.length) + 500);
       break;
 
     case "project3":
       passedCommands.unshift(cmd);
       var text = [
         "<h3>Sorting Algorithm Visualizer</h3>",
-        "A Website using ReactJs to show the user a visual representation of many",
-        "sorting algorithms: Bubble Sort, Merge Sort, Quick Sort, Insertion Sort, Selection Sort",
+        "A Website to show the user a visual representation of many sorting algorithms: ",
+        "Bubble Sort, Merge Sort, Quick Sort, Insertion Sort, Selection Sort",
+        "-> Created a frontend using ReactJS and CSS",
+        "-> Utilized CSS to dynamically create the height of bars",
+        "-> Coded Sorting Algorithms in JavaScript",
         "Code: <a href='https://github.com/RithvikKasarla/sorting-visualizer'>https://github.com/RithvikKasarla/sorting-visualizer</a>",
         "Published: <a href=https://rithvikkasarla.github.io/sorting-visualizer/>https://rithvikkasarla.github.io/sorting-visualizer/</a>",
         "Youtube Video:<a href= https://www.youtube.com/watch?v=ldOwCBbZcIM>https://www.youtube.com/watch?v=ldOwCBbZcIM</a>",
@@ -256,19 +267,25 @@ function outputs(cmd) {
       addLines(text);
       setTimeout(() => {
         addNewCommandLine();
-      }, lineTime(text, text.length));
+      }, lineTime(text, text.length) + 500);
       break;
     case "project4":
       passedCommands.unshift(cmd);
       var text = [
-        "<h3>Comment Anywhere App</h3>",
-        "A chrome extension	that allows the user to comment on any website and also see",
-        "other users comments.",
+        "<h3>Freezepond Chrome extension</h3>",
+        "Developed as a part of the hackathon over a weekend, the chrome extension and website ",
+        "allow users to share comments about a webpage which other users can see. This",
+        "effectively allows the community to see honest information of websites that they are on. ",
+        "-> Created the idea",
+        "-> Created a VueJs frontend of the chrome extension ",
+        "-> Designed and helped develop the website,",
+        "-> Setup the SQL database",
+        "Tech: VueJs, JavaScript, HTML, C++ ",
       ];
       addLines(text);
       setTimeout(() => {
         addNewCommandLine();
-      }, lineTime(text, text.length) + 90);
+      }, lineTime(text, text.length) + 600);
       break;
     case "project5":
       passedCommands.unshift(cmd);
@@ -276,23 +293,33 @@ function outputs(cmd) {
         "<h3>Article Summarizer</h3>",
         "A chrome extension using NLP, Natural Language Processing, in order to",
         "summarize an article that the user is currently on.",
+        "-> Created the Chrome extension frontend",
+        "-> Developed the connection between the chrome extension and webpage",
+        "-> Extracted text utilizing the mercury-parcer library",
+        "-> Utilized the BART model trained on the CNN/DailyMail dataset to summarize",
+        "Tech: HTML, CSS, JavaScript, BART model, mercury-parcer library",
       ];
       addLines(text);
       setTimeout(() => {
         addNewCommandLine();
-      }, lineTime(text, text.length)) + 50;
+      }, lineTime(text, text.length) + 500);
       break;
     case "project6":
       passedCommands.unshift(cmd);
       var text = [
         "<h3>Water Quality Predictor</h3>",
         "	A program using Sklearn which uses past chemical composition data of water in",
-        "california and predicts the chemical composition into the future using forecasting.",
+        "california and predicts the chemical composition into the future.",
+        "-> Called water companies to collect water quality data",
+        "-> Formatted the data using pandas an numpy",
+        "-> Trained a forcasting model utilizing sklearn algorithms",
+        "-> Plotted the trend with Matplotlib",
+        "Tech: Python, numpy, pandas, matplotlib, Sklearn",
       ];
       addLines(text);
       setTimeout(() => {
         addNewCommandLine();
-      }, lineTime(text, text.length));
+      }, lineTime(text, text.length) + 500);
       break;
     case "whois":
       passedCommands.unshift(cmd);
